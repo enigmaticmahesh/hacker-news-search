e9dfd76b33dc2c890776a57474b93c9816f4862f
enigmaticmahesh <challengermahesh@gmail.com>
2022-11-07 21:29:34 +0530

    Https issue fixed

--- src/common/api.js
+++ src/common/api.js
@@ -1,4 +1,4 @@
 export const API = {
   getNews: 'https://hn.algolia.com/api/v1/search_by_date',
-  searchNews: 'http://hn.algolia.com/api/v1/search',
+  searchNews: 'https://hn.algolia.com/api/v1/search',
 };
